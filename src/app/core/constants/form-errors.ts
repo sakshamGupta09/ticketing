ddb4d3a9e9342737dd9cdd55009a2d1c5e739981
Saksham Gupta <saksham.09.gupta@gmail.com>
2023-04-13 07:09:31 +0530

    Forgot password static UI

--- src/app/core/constants/form-errors.ts
+++ src/app/core/constants/form-errors.ts
@@ -1,8 +1,9 @@
 const ERROR_MESSAGES: Record<string, string> = {
-  emailRequired    : 'Email is required',
-  invalidEmail     : 'Please enter a valid email',
-  passwordRequired : 'Password is required',
-  loginFailed      : 'Invalid email or password'
+  emailRequired        : 'Email is required',
+  invalidEmail         : 'Please enter a valid email',
+  passwordRequired     : 'Password is required',
+  loginFailed          : 'Invalid email or password',
+  forgotPasswordFailed : 'If you have a valid account, password recovery instructions have been sent to the registered email address.'
 };
 
 export default ERROR_MESSAGES;
--- src/app/public/components/forgot-password/forgot-password.component.ts
+++ src/app/public/components/forgot-password/forgot-password.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ERROR_MESSAGES from 'src/app/core/constants/form-errors';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
+  public form: FormGroup = {} as FormGroup;
 
+  public isLoading: boolean = false;
+
+  readonly formErrors = ERROR_MESSAGES;
+
+  public emailVerificationFailed: boolean = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      return this.form.markAllAsTouched();
+    }
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+      this.emailVerificationFailed = true;
+    }, 3000);
+  }
 }
