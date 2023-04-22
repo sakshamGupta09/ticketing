f707fd30517fccc1cb363fa6227c9b975fde589c
Saksham Gupta <saksham.09.gupta@gmail.com>
2023-04-22 06:49:04 +0530

    Exclude services from test coverage

--- jest.config.js
+++ jest.config.js
@@ -15,10 +15,11 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
+      branches: 90,
+      functions: 90,
+      lines: 90,
       statements: -10,
     },
   },
+  coveragePathIgnorePatterns: ['.service.ts'],
 };
