60c9b9cf30abbb73473f31e594c0b27441739280
GUPTA SAKSHAM <c11532b@cnh1.cnhgroup.cnh.com>
2023-06-04 16:51:00 +0530

    Tests for layout

---
+++ src/app/private/layout/layout.component.spec.ts
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
+import { TestBed } from '@angular/core/testing';
+import { createMock } from '@testing-library/angular/jest-utils';
+
+import { TestModule } from '../../../tests/test.module';
+
+import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BackdropComponent } from './components/backdrop/backdrop.component';
+
+describe('LayoutComponent', () => {
+  test('it should render header, sidebar and main', async () => {
+    await render(LayoutComponent, {
+      declarations: [SidebarComponent, HeaderComponent, BackdropComponent],
+      imports: [TestModule],
+    });
+
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+    expect(screen.getByRole('banner')).toHaveClass('header');
+
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+    expect(screen.getByRole('presentation')).toHaveClass('sidebar');
+
+    expect(screen.getByRole('main')).toBeInTheDocument();
+    expect(screen.getByRole('main')).toHaveClass('layout__content');
+  });
+
+  test('toggle sidebar should be functional', async () => {
+    const { fixture } = await render(LayoutComponent, {
+      componentProperties: {
+        isSidebarOpen: true,
+      },
+      declarations: [SidebarComponent, HeaderComponent, BackdropComponent],
+      imports: [TestModule],
+    });
+
+    fixture.componentInstance.toggleSidebar();
+    expect(fixture.componentInstance.isSidebarOpen).toEqual(false);
+  });
+});
