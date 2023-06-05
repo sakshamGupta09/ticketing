75959389a8956f0e89e9f5fa65d1d344dcb73a76
GUPTA SAKSHAM <c11532b@cnh1.cnhgroup.cnh.com>
2023-06-05 06:50:34 +0530

    CSS fixes

--- src/app/private/users/users.module.ts
+++ src/app/private/users/users.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatTableModule } from '@angular/material/table';
+
 import { UsersRoutingModule } from './users-routing.module';
 import { ListComponent } from './components/list/list.component';
 
@@ -9,7 +11,7 @@ import { UsersService } from './services/users.service';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule, HttpClientModule, UsersRoutingModule],
+  imports: [CommonModule, HttpClientModule, UsersRoutingModule, MatTableModule],
   providers: [UsersService],
 })
 export class UsersModule {}
