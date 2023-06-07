5ebd295bfa4af6ad4b65988d27e4cf6839033e17
GUPTA SAKSHAM <c11532b@cnh1.cnhgroup.cnh.com>
2023-06-07 17:44:01 +0530

    Tests for drawer component

--- src/app/shared/drawer/drawer.component.spec.ts
+++ src/app/shared/drawer/drawer.component.spec.ts
@@ -1,23 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render, screen, within } from '@testing-library/angular';
 import { DrawerComponent } from './drawer.component';
+import { TestModule } from '../../../tests/test.module';
 
 describe('DrawerComponent', () => {
-  let component: DrawerComponent;
-  let fixture: ComponentFixture<DrawerComponent>;
+  test('it should render the drawer based on Input property', async () => {
+    await render(DrawerComponent, {
+      componentInputs: { isVisible: false },
+      imports: [TestModule],
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DrawerComponent ]
-    })
-    .compileComponents();
+    expect(screen.getByRole('complementary')).toBeInTheDocument();
+    expect(screen.getByRole('complementary')).not.toHaveClass('drawer--open');
 
-    fixture = TestBed.createComponent(DrawerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    expect(screen.queryByRole('presentation')).toBeNull();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  test('it should render drawer in opened state if input is truthy', async () => {
+    await render(DrawerComponent, {
+      componentInputs: { isVisible: true },
+      imports: [TestModule],
+    });
+
+    expect(screen.getByRole('complementary')).toBeInTheDocument();
+    expect(screen.getByRole('complementary')).toHaveClass('drawer--open');
+
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+    expect(screen.getByRole('presentation')).toHaveClass('drawer-overlay');
+  });
+
+  test('it should render projected content', async () => {
+    const projection = 'it should be srendered inside drawer!';
+
+    await render(`<app-drawer [isVisible]="true">${projection}</app-drawer>`, {
+      declarations: [DrawerComponent],
+      imports: [TestModule],
+    });
+
+    const drawerElement = screen.getByRole('complementary');
+
+    expect(screen.getByText(projection)).toBeInTheDocument();
+    expect(within(drawerElement).getByText(projection)).toBeInTheDocument();
   });
 });
