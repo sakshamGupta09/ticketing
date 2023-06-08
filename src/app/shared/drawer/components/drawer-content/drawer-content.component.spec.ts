5bfe29c2262fa92313cd5fe8b8c04815f46565ed
GUPTA Saksham (CNH Industrial) <c11532b@cnh1.cnhgroup.cnh.com>
2023-06-08 10:55:22 +0530

    Tests for drawer-content components

--- src/app/shared/drawer/components/drawer-content/drawer-content.component.spec.ts
+++ src/app/shared/drawer/components/drawer-content/drawer-content.component.spec.ts
@@ -1,23 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { render, screen, within } from '@testing-library/angular';
+import { TestModule } from '../../../../../tests/test.module';
 import { DrawerContentComponent } from './drawer-content.component';
+import userEvent from '@testing-library/user-event';
 
 describe('DrawerContentComponent', () => {
-  let component: DrawerContentComponent;
-  let fixture: ComponentFixture<DrawerContentComponent>;
+  test('it should render header, body and footer elements', async () => {
+    await render(DrawerContentComponent, {
+      imports: [TestModule],
+    });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DrawerContentComponent ]
-    })
-    .compileComponents();
+    expect(screen.getByTestId('drawer-header')).toBeInTheDocument();
+    expect(screen.getByTestId('drawer-header')).toHaveClass(
+      'drawer__header flex justify-between gap-2 items-center'
+    );
 
-    fixture = TestBed.createComponent(DrawerContentComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    expect(screen.getByTestId('drawer-body')).toBeInTheDocument();
+    expect(screen.getByTestId('drawer-body')).toHaveClass(
+      'drawer__body flex-grow overflow-y-auto'
+    );
+
+    expect(screen.getByTestId('drawer-footer')).toBeInTheDocument();
+    expect(screen.getByTestId('drawer-footer')).toHaveClass('drawer__footer');
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  test('close drawer button should be functional', async () => {
+    const mockFn = jest.fn();
+
+    await render(DrawerContentComponent, {
+      componentOutputs: {
+        closeClicked: { emit: mockFn } as any,
+      },
+      imports: [TestModule],
+    });
+
+    const closeButton = screen.getByRole('button');
+    expect(closeButton).toBeInTheDocument();
+
+    await userEvent.click(closeButton);
+
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  test('content projection should be functional', async () => {
+    const modalTitle = 'Modal header';
+    await render(
+      `<app-drawer-content>
+        <ng-container title>${modalTitle}</ng-container>
+        <ng-container body>${modalTitle}</ng-container>
+        <ng-container footer>${modalTitle}</ng-container>
+
+      </app-drawer-content>`,
+      { declarations: [DrawerContentComponent], imports: [TestModule] }
+    );
+
+    expect(
+      screen.getByRole('heading', { name: modalTitle })
+    ).toBeInTheDocument();
+
+    const drawerBody = screen.getByTestId('drawer-body');
+    const drawerFooter = screen.getByTestId('drawer-footer');
+
+    expect(within(drawerBody).getByText(modalTitle)).toBeInTheDocument();
+    expect(within(drawerFooter).getByText(modalTitle)).toBeInTheDocument();
   });
 });
